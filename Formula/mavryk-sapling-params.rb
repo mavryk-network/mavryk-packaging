b61688e57c3db51287f52edc6f65c36c356092b0
MavrykCowbot <info@mavryk.io>
2024-07-23 03:25:31 -0700

    Mavryk Sapling Params fixed

--- Formula/mavryk-sapling-params.rb
+++ Formula/mavryk-sapling-params.rb
@@ -4,11 +4,16 @@
 class MavrykSaplingParams < Formula
   homepage "https://github.com/mavryk-network/mavryk-packaging"
   
-  url "https://gitlab.com/tezos/opam-repository.git", :tag => "v8.2", :shallow => false
+  url "https://gitlab.com/tezos/opam-repository.git", :tag => "v9.4", :shallow => false
 
   version "v20.2-rc1"
 
   desc "Sapling params required at runtime by the Mavryk binaries"
+
+  bottle do
+    root_url "https://github.com/mavryk-network/mavryk-packaging/releases/download/#{MavrykSaplingParams.version}/"
+    sha256 cellar: :any, monterey: "51253fd0d0fdb9c8e0adfdd753be4184d56ed53b64c5b79fb1010520f063961c"
+  end
 
   def install
     share.mkpath
