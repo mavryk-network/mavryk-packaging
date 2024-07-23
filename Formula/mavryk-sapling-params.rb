5620d75c2f14e572a05da187468602bd4a116824
MavrykCowbot <info@mavryk.io>
2024-07-23 11:51:11 +0200

    Mavryk Sapling Params test update

--- Formula/mavryk-sapling-params.rb
+++ Formula/mavryk-sapling-params.rb
@@ -2,8 +2,9 @@
 # SPDX-License-Identifier: LicenseRef-MIT-OA
 
 class MavrykSaplingParams < Formula
-  url "https://gitlab.com/tezos/opam-repository.git", :tag => "v8.2"
   homepage "https://github.com/mavryk-network/mavryk-packaging"
+  
+  url "https://gitlab.com/tezos/opam-repository.git", :tag => "v8.2", :shallow => false
 
   version "v20.2-rc1"
 
