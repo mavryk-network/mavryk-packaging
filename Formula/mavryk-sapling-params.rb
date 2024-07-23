adb81998edff77dcde1898dca76d8ddfbc119e35
MavrykCowbot <info@mavryk.io>
2024-07-23 11:40:48 +0200

    Mavryk Sapling Params test update

--- Formula/mavryk-sapling-params.rb
+++ Formula/mavryk-sapling-params.rb
@@ -1,13 +1,10 @@
 # SPDX-FileCopyrightText: 2021 Oxhead Alpha
 # SPDX-License-Identifier: LicenseRef-MIT-OA
 
-# TODO: once there is a new release of opam-repository this should be updated
 class MavrykSaplingParams < Formula
   url "https://gitlab.com/tezos/opam-repository.git", :tag => "v8.2"
   homepage "https://github.com/mavryk-network/mavryk-packaging"
-
   version "v20.2-rc1"
-
   desc "Sapling params required at runtime by the Mavryk binaries"
 
   bottle do
